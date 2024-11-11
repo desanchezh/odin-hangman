219723df8905cf116abfcb842e2d70299579c43e
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-11-11 16:32:02 -0500

    Basic game functionality complete

--- lib/game.rb
+++ lib/game.rb
@@ -1,33 +1,44 @@
 # 7 tries
 class Game
-  def initialize(dictionary)
+  def initialize
     puts 'game initialized'
-    @random_word = get_random_word(dictionary)
     @incorrect_guesses = []
     @correct_guesses = []
+    @remaining_guesses = 7
   end
 
-  attr_reader :correct_guesses, :incorrect_guesses
+  attr_reader :correct_guesses, :incorrect_guesses, :remaining_guesses, :random_word
 
   def get_random_word(dictionary)
     all_words = []
     dictionary.each do |word|
       clean_word = word.to_s.chomp
       all_words << clean_word if clean_word.length >= 5 && clean_word.length <= 12
-      all_words.sample
     end
+    all_words.sample
   end
 
   def check_guess(guess_letter, random_word)
-    p random_word_letters = random_word.split('')
-    @incorrect_guesses << guess_letter unless random_word_letters.include?(guess_letter)
-    random_word_letters.each_with_index do |random_letter, i|
-      @correct_guesses[i] = if random_letter == guess_letter
-                              random_letter
-                            else
-                              '_'
-                            end
+    random_word_letters = random_word.split('')
+    if !random_word_letters.include?(guess_letter)
+      @incorrect_guesses << guess_letter
+      @remaining_guesses -= 1
+      puts "wrong. incorrect guesses so far: #{@incorrect_guesses.join(' ')}"
+      puts "you have #{@remaining_guesses} guesses remaining"
+      puts "letters found: #{@correct_guesses.join(' ')}"
+    elsif random_word_letters.include?(guess_letter)
+      random_word_letters.each_with_index do |random_letter, i|
+        if random_letter == guess_letter
+          correct_guesses[i] = random_letter
+        elsif @correct_guesses[i].nil? || @correct_guesses[i] == '_'
+          correct_guesses[i] = '_'
+        else
+          @correct_guesses[i]
+        end
+      end
+      puts 'correct letter!'
+      puts "letters found: #{@correct_guesses.join(' ')}"
     end
-    @correct_guesses
+    @correct_guesses = correct_guesses
   end
 end
--- main.rb
+++ main.rb
@@ -2,8 +2,18 @@ require_relative('lib/game')
 
 dictionary = File.new('google-10000-english-no-swears.txt', 'r')
 
-game = Game.new(dictionary)
+game = Game.new
+p random_word = game.get_random_word(dictionary)
+while game.remaining_guesses.positive?
 
-game.check_guess('a', 'random')
-
-p game.correct_guesses
+  if game.correct_guesses.join('') == random_word
+    puts 'you win!'
+    puts "the word was: #{random_word}"
+    exit
+  else
+    puts 'Guess a letter'
+    letter = gets.chomp.downcase
+    game.check_guess(letter, random_word)
+  end
+end
+puts 'Game over. No more guesses remaining'
