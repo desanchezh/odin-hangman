9e6593178a0e887172b2bd3f836985cd45779115
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-11-11 12:23:53 -0500

    Add min and amx length to random word

--- main.rb
+++ main.rb
@@ -2,9 +2,9 @@ dictionary = File.new('google-10000-english-no-swears.txt', 'r')
 
 def get_random_word(dictionary)
   all_words = []
-  contents.each do |word|
+  dictionary.each do |word|
     clean_word = word.to_s.chomp
-    all_words << clean_word
+    all_words << clean_word if clean_word.length >= 5 && clean_word.length <= 12
   end
   all_words.sample
 end
